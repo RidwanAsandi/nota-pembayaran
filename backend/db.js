592b3b5ad9cb86e9f20ecc0be4112b45fc265795
RidwanAsandi <ridwanasandi09@gmail.com>
2025-04-07 02:31:53 +0700

    ubah db

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "postgres.railway.internal",
-  database: "railway",
-  password: "cBGfLfIoYiENZEIynbMhrttbKdSgMMaS", // ganti sesuai password
+  host: "localhost",
+  database: "notadb",
+  password: "root", // ganti sesuai password,
   port: 5432,
 });
 
